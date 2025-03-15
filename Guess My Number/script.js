e0ad5e5295f2a4e534e7411e3715da9bb4c23f3d
Gerard Causse <grod.causse@gmail.com>
2025-03-15 17:36:25 -0400

    Refactor

--- Guess My Number/script.js
+++ Guess My Number/script.js
@@ -4,62 +4,66 @@ let highScore = 0;
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 
-document.querySelector(".check").addEventListener("click", function () {
+const displayMessage = (message) =>
+  (document.querySelector(".message").textContent = message);
+const displayScore = (score) =>
+  (document.querySelector(".score").textContent = score);
+
+document.querySelector(".check").addEventListener("click", () => {
   const guess = Number(document.querySelector(".guess").value);
-  console.log(guess);
 
   // When there is no input
   if (!guess) {
-    document.querySelector(".message").textContent = "⛔ No number!";
+    displayMessage("⛔ No number!");
   } else if (guess === secretNumber) {
     // Correct guess
-    document.querySelector(".message").textContent = "🎉 Correct Number!";
-    document.querySelector(".number").textContent = secretNumber;
-    document.querySelector("body").style.backgroundColor = "#60b347";
-    document.querySelector(".number").style.width = "30rem";
+    const VICTORY_BOX_WIDTH = "30rem";
+    const VICTORY_COLOR = "#60b347";
 
+    // Update UI
+    displayMessage("🎉 Correct Number!");
+    document.querySelector(".number").textContent = secretNumber;
+    document.querySelector(".number").style.width = VICTORY_BOX_WIDTH;
+    document.querySelector("body").style.backgroundColor = VICTORY_COLOR;
+
+    // New high score
     if (score > highScore) {
       highScore = score;
       document.querySelector(".highscore").textContent = highScore;
     }
-    // Guess too high
-  } else if (guess > secretNumber) {
-    if (score > 1) {
-      // Miss
-      document.querySelector(".message").textContent = "📈 Too high!";
-      score--;
-      document.querySelector(".score").textContent = score;
-    } else {
-      // Game over
-      document.querySelector(".message").textContent = "💥 You lost!";
-      document.querySelector(".score").textContent = 0;
-    }
-    // Guess too low
+    // Incorrect guess
   } else {
-    // Miss
+    // Game continues
     if (score > 1) {
-      document.querySelector(".message").textContent = "📉 Too low!";
-      score--;
-      document.querySelector(".score").textContent = score;
+      // Update UI
+      displayMessage(guess > secretNumber ? "📈 Too high!" : "📉 Too low!");
+      displayScore(--score);
     } else {
       // Game over
-      document.querySelector(".message").textContent = "💥 You lost!";
-      document.querySelector(".score").textContent = 0;
+      // Alert user to game over
+      displayMessage("💥 You lost!");
+      displayScore("0");
     }
   }
 });
 
 // Select and attach click handler to "Again" button
-document.querySelector(".again").addEventListener("click", function () {
+document.querySelector(".again").addEventListener("click", () => {
+  const DEFAULT_COLOR = "#222";
+  const DEFAULT_BOX_WIDTH = "15rem";
+  const UNKNOWN_NUMBER_MESSAGE = "?";
+
   // Restore initial values
   secretNumber = Math.trunc(Math.random() * 20) + 1;
   score = 20;
+
   // Restore initial condition of message fields
-  document.querySelector(".number").textContent = "?";
+  document.querySelector(".number").textContent = UNKNOWN_NUMBER_MESSAGE;
   document.querySelector(".score").textContent = score;
-  document.querySelector(".guess").value = null;
-  document.querySelector(".message").textContent = "Start guessing...";
+  document.querySelector(".guess").value = "";
+  displayMessage("Start guessing...");
+
   // Restore original styles
-  document.querySelector("body").style.backgroundColor = "#222";
-  document.querySelector(".number").style.width = "15rem";
+  document.querySelector("body").style.backgroundColor = DEFAULT_COLOR;
+  document.querySelector(".number").style.width = DEFAULT_BOX_WIDTH;
 });
