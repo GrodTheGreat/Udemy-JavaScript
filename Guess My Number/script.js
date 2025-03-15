16b46c49c6149f81822e5e3174bfdad26e3563ce
Gerard Causse <grod.causse@gmail.com>
2025-03-15 15:06:42 -0400

    Complete Manipulating CSS Styles

--- Guess My Number/script.js
+++ Guess My Number/script.js
@@ -9,12 +9,17 @@ document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   console.log(guess);
 
+  // When there is no input
   if (!guess) {
     document.querySelector(".message").textContent = "⛔ No number!";
   } else if (guess === secretNumber) {
+    // Correct guess
     document.querySelector(".message").textContent = "🎉 Correct Number!";
+    document.querySelector("body").style.backgroundColor = "#60b347";
+    document.querySelector(".number").style.width = "30rem";
   } else if (guess > secretNumber) {
     if (score > 1) {
+      // Guess to high
       document.querySelector(".message").textContent = "📈 Too high!";
       score--;
       document.querySelector(".score").textContent = score;
@@ -23,6 +28,7 @@ document.querySelector(".check").addEventListener("click", function () {
       document.querySelector(".score").textContent = 0;
     }
   } else {
+    // Guess too low
     if (score > 1) {
       document.querySelector(".message").textContent = "📉 Too low!";
       score--;
